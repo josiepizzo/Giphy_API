d714233f37ee53082292b5bf422398054419da08
judith pizzolato <judith pizzolato>
2016-06-18 00:34:39 -0400

    added styling

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,39 +1,37 @@
 // Initial array of animals
-	var animals = ['cat', 'dog', 'bird'];
-		
+	var animals = ['cat', 'dog', 'bird'];	
 
 	$('body').on('click', '.animal', function displayAnimalInfo(){
 		$("#gifsAppearHere").empty();
 		var animal = $(this).attr('data-name');
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    $.ajax({
-                url: queryURL,
-                method: 'GET'
+    $.ajax({url: queryURL,
+            method: 'GET'
             })
             .done(function(response) {
-                var results = response.data;
-                console.log(response)
+              var results = response.data;
+              console.log(response)
 
-                    for (var i = 0; i < results.length; i++) {
-                    var animalDiv = $('<div>');
-                    var p = $('<p>');
+              for (var i = 0; i < results.length; i++) {
+                  var animalDiv = $('<div>');
+                  var p = $('<p>');
+                  console.log(results[i].rating);
+                  p.text(results[i].rating);
 
-                    console.log(results[i].rating);
-                    p.text(results[i].rating);
+                  var animalImage = $('<img>');
+                  animalImage.attr('src', results[i].images.fixed_height.url);
+                  animalDiv.append(p);
+                  animalDiv.append(animalImage);
 
-                    var animalImage = $('<img>');
-                    animalImage.attr('src', results[i].images.fixed_height.url);
+              		$('#gifsAppearHere').prepend(animalDiv);
 
-                    animalDiv.append(p);
-                    animalDiv.append(animalImage);
+              		$(document).ready(function() {
+              		$('.animalDiv').css('background-color','#d43f3a');
+              	});
 
-                    $('#gifsAppearHere').prepend(animalDiv);
                 }
-
-            });                
-
-		//------
+            });
 	});
 
 	// ========================================================
@@ -46,10 +44,14 @@
 			// Then dynamicaly generates buttons for each animal in the array
 			// Note the jQUery syntax here... 
 		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-		    a.addClass('animal'); // Added a class 
+		    a.addClass('animal btn btn-primary btn-sm'); // Added a class 
 		    a.attr('data-name', animals[i]); // Added a data-attribute
 		    a.text(animals[i]); // Provided the initial button text
 		    $('#buttonsView').append(a); // Added the button to the HTML
+
+		    // $(document).ready(function() {
+      //         		$('.animal).css('background-color','yellow');
+      //         	});
 		}
 	}
 	// ========================================================
